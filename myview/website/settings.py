d036703be284ca681587212bbb7401899b58c21f
EdyDaSilva <ednedson@hotmail.com>
2024-02-23 09:17:25 +0000

    settings debug back to True and no allowed host

--- myview/website/settings.py
+++ myview/website/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q3*t10od$!c0awb58ux=dr^m9*2p%cf@2%yn_hrd)^)51!3^gc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['www.edydasilva.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
